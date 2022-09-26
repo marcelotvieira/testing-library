b4bff454c7d607e3f8a54a9d50293ccaeb1760db
Marcelo Vieira <marcelodefy15@gmail.com>
2022-09-26 16:32:31 -0300

    req4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando a página notFound', () => {
+  test('Testando se a página exibe o título', () => {
+    renderWithRouter(<NotFound />);
+    const expectedHeading = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2 });
+    expect(expectedHeading).toBeInTheDocument();
+  });
+  test('Testando se a imagem está sendo exibida', () => {
+    renderWithRouter(<NotFound />);
+    const expectedImage = screen.getByRole('img');
+    const expectedURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(expectedImage.src).toContain(expectedURL);
+  });
+});
